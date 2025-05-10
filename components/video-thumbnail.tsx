e0a3262a4066fe1b5d68944473ad3e3db14d7857
Marcel Fahle <m.fahle@gmail.com>
2025-05-10 08:00:50 +0200

    fix: better thumbnail linking

--- components/video-thumbnail.tsx
+++ components/video-thumbnail.tsx
@@ -28,37 +28,42 @@ export function VideoThumbnail({
     <div
       className={`flex ${flexDirection} items-start gap-12 mb-12 w-full  mx-auto`}
     >
-      {/* Thumbnail */}
-      <div className="w-2/5 min-w-[180px] aspect-video relative overflow-hidden rounded-lg">
-        <Image
-          src={video.thumbnail}
-          alt={video.title}
-          fill={true}
-          className="object-cover"
-          sizes="100vw, (max-width: 640px) 640px"
-        />
-        <span className="bg-black text-white absolute px-2 py-1 font-semibold text-sm bottom-3 right-3 rounded-md">
-          {formatDuration(video.duration)}
-        </span>
-      </div>
-      {/* Info */}
-      <div className="flex-1">
-        <h3
-          className={`${spaceGrotesk.className} font-semibold text-2xl mb-2 ${titleClassName}`}
-        >
-          <Link href={`/v/${video.id}`} prefetch={prefetch}>
+      <Link
+        href={`/v/${video.id}`}
+        prefetch={prefetch}
+        className="flex w-full"
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        {/* Thumbnail */}
+        <div className="w-2/5 min-w-[180px] aspect-video relative overflow-hidden rounded-lg">
+          <Image
+            src={video.thumbnail}
+            alt={video.title}
+            fill={true}
+            className="object-cover"
+            sizes="100vw, (max-width: 640px) 640px"
+          />
+          <span className="bg-black text-white absolute px-2 py-1 font-semibold text-sm bottom-3 right-3 rounded-md">
+            {formatDuration(video.duration)}
+          </span>
+        </div>
+        {/* Info */}
+        <div className="flex-1">
+          <h3
+            className={`${spaceGrotesk.className} font-semibold text-2xl mb-2 ${titleClassName}`}
+          >
             {video.title}
-          </Link>
-        </h3>
-        {video.teaser && (
-          <p className="mb-2 text-base font-[adaptive-mono] text-foreground">
-            {video.teaser}
+          </h3>
+          {video.teaser && (
+            <p className="mb-2 text-base font-[adaptive-mono] text-foreground">
+              {video.teaser}
+            </p>
+          )}
+          <p className="text-[adaptive-mono] mt-8 text-sm">
+            {formatRelative(new Date(video.published_at), new Date())}
           </p>
-        )}
-        <p className="text-[adaptive-mono] mt-8 text-sm">
-          {formatRelative(new Date(video.published_at), new Date())}
-        </p>
-      </div>
+        </div>
+      </Link>
     </div>
   );
 }
