5f03998da43e7542b1fe5909b095b7142ddae559
Marcel Fahle <m.fahle@gmail.com>
2025-05-10 15:14:32 +0200

    chore: mobile list view

--- components/video-thumbnail.tsx
+++ components/video-thumbnail.tsx
@@ -22,18 +22,19 @@ export function VideoThumbnail({
   align = "left",
   titleClassName = "",
 }: VideoThumbnailProps) {
-  // Use flex-row or flex-row-reverse based on align
-  const flexDirection = align === "right" ? "flex-row-reverse" : "flex-row";
+  // Use responsive flex direction
+  const flexDirection =
+    align === "right" ? "sm:flex-row-reverse" : "sm:flex-row";
   return (
-    <div className={"mb-12 w-full mx-auto"}>
+    <div className={"mb-8 sm:mb-12 w-full mx-auto"}>
       <Link
         href={`/v/${video.id}`}
         prefetch={prefetch}
-        className={`flex ${flexDirection} items-start gap-12 w-full`}
+        className={`flex flex-col ${flexDirection} items-start sm:gap-12 gap-4 w-full`}
         style={{ textDecoration: "none", color: "inherit" }}
       >
         {/* Thumbnail */}
-        <div className="w-2/5 min-w-[180px] aspect-video relative overflow-hidden rounded-lg">
+        <div className="w-full sm:w-2/5 min-w-0 aspect-video relative overflow-hidden rounded-lg">
           <Image
             src={video.thumbnail}
             alt={video.title}
@@ -41,23 +42,23 @@ export function VideoThumbnail({
             className="object-cover"
             sizes="100vw, (max-width: 640px) 640px"
           />
-          <span className="bg-black text-white absolute px-2 py-1 font-semibold text-sm bottom-3 right-3 rounded-md">
+          <span className="bg-black text-white absolute px-2 py-1 font-semibold text-xs sm:text-sm bottom-2 sm:bottom-3 right-2 sm:right-3 rounded-md">
             {formatDuration(video.duration)}
           </span>
         </div>
         {/* Info */}
-        <div className="flex-1">
+        <div className="flex-1 w-full">
           <h3
-            className={`${spaceGrotesk.className} font-semibold text-2xl mb-2 ${titleClassName}`}
+            className={`${spaceGrotesk.className} font-semibold text-lg sm:text-2xl mb-1 sm:mb-2 ${titleClassName}`}
           >
             {video.title}
           </h3>
           {video.teaser && (
-            <p className="mb-2 text-base font-[adaptive-mono] text-foreground">
+            <p className="mb-1 sm:mb-2 text-sm sm:text-base font-[adaptive-mono] text-foreground">
               {video.teaser}
             </p>
           )}
-          <p className="text-[adaptive-mono] mt-8 text-sm">
+          <p className="text-[adaptive-mono] mt-4 sm:mt-8 text-xs sm:text-sm">
             {formatRelative(new Date(video.published_at), new Date())}
           </p>
         </div>
